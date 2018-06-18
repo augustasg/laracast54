2841a6dc3e983e01af85f6efb5ecbc306f46bf5b
Augustas <augustas@koralis.lt>
2018-06-18 15:02:44 +0300

    finished ep10

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+    <h1>One more post</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::get('/tasks', 'TasksController@index');
 Route::get('/tasks/{task}','TasksController@show');
 
 Route::get('/posts', 'PostsController@index');
+Route::get('/posts/{post}', 'PostsController@show');
 
 // controller => PostsController
 // Eloquent model => Post
